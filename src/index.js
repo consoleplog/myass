f0e695b93cee6cd2526c586b80b1b47f3fd43b1a
consoleplog <824923125@qq.com>
2016-09-23 09:46:28 +0800

    gh-pages bushu

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {render} from 'react-dom';
-import App from './App.js';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Routers from './routers'
-import './main.css'
-import 'highlight.js/styles/github.css';
-import './post.css';
-render(<Routers />,document.getElementById('root'));
